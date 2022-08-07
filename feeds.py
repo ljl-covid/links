886ef70ea0f55e3ea75b6a344b667210a3411c44
ljl-covid <62215844+ljl-covid@users.noreply.github.com>
2022-08-08 01:08:40 +0200

    Fill ##monkeypox up a bit more

--- feeds.py
+++ feeds.py
@@ -251,7 +251,7 @@ feeds.update({
             "https://tools.cdc.gov/api/v2/resources/media/403372.rss": "CDC (old)*",
             "https://feeds.feedburner.com/who/euro/rss/en": "WHO Euro",
             "https://www.who.int/feeds/entity/mediacentre/news/en/rss.xml": "WHO",
-#            "https://old.reddit.com/r/worldnews/rising.rss?limit=3": "r/WorldNews",
+            "https://old.reddit.com/r/worldnews/rising.rss?limit=10": "r/WorldNews",
             "https://old.reddit.com/r/science/.rss": "r/Science",
             "https://feeds.feedburner.com/RsoeEdis-Ebhwm?format=xml": "RSOE EDIS",
             "https://feeds.newscientist.com/health": "New Scientist",
